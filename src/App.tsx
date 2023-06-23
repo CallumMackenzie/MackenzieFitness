167d44e7127ef3a00c05c53bad2620ca8c0a779a
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2023-06-23 11:38:24 -0700

    Working on linking firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
+	measurementId: process.env.REACT_APP_measurementId
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
+	return (<div className='container-fluid'>
+		<div className="row">
+			<header>
+				<h1>Welcome to Mackenzie Fitness!</h1>
 			</header>
 		</div>
-	);
+		<div className='row'>
+		</div>
+		<div className='row'>
+		</div>
+	</div>);
 }
 
 export default App;
